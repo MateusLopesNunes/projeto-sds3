afbb2e66a738099477c2c495b42087279759fb32
MateusLopesNunes <mateuslopesnunes@outlook.com>
2021-09-10 14:52:15 -0300

    First request

--- frontend/src/components/DonutCharts/index.tsx
+++ frontend/src/components/DonutCharts/index.tsx
@@ -1,6 +1,26 @@
+import axios from 'axios';
 import Chart from 'react-apexcharts';
+import { SaleSum } from 'types/sale';
+import { Base_URL } from '../../utils/requests';
+
+type ChartData = {
+    series: number[];
+    labels: string[];
+}
 
 const DonutCharts = () => {
+
+    let chartData: ChartData = {series: [], labels: []}
+
+    axios.get(`${Base_URL}/sales/sum-by-seller`).then(response => {
+        const data = response.data as SaleSum[];
+        const myLabels = data.map(x => x.sellerName);
+        const mySeries = data.map(x => x.sum);
+        
+        chartData = {series: mySeries, labels: myLabels};
+        console.log(chartData);
+    });
+
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
