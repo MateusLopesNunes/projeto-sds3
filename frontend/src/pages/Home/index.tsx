5568fd8588fca7a1868d501f3b55a0592ecdba95
MateusLopesNunes <mateuslopesnunes@outlook.com>
2021-09-10 10:45:17 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
+import Footer from "../../components/Footer";
+import NavBar from "../../components/NavBar";
+
 const Home = () => {
     return (
-        <h1>Página home</h1>
+        <>
+        <NavBar/>
+        <div className="container">
+            <div className="jumbotron">
+                <h1 className="display-4">DSVendas</h1>
+                <p className="lead">Analise o desempenho das suas vendas por diferentes perspectivas</p>
+                <hr />
+                <p>Esta aplicação consiste em exibir um dashboard a partir de dados fornecidos por um back end construído com Spring Boot.</p>
+            </div>
+            <Link className="btn btn-primary" to="/dashboard">
+                Acessar dashboard
+            </Link>
+        </div> 
+        <Footer/>
+        </>
+        
     );
 }
 
